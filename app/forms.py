a04f869dd5b0761c3a080b5fbc55cbd159ac95c5
Lei Zhen <1027224454@qq.com>
2019-10-24 17:07:50 +0800

    做表单验证 validate_on_submit() 判断是否需要验证

---
+++ app/forms.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+# @Time    : 19/10/24 15:10
+# @Author  : Lei Zhen
+# @Contract: leizhen8080@gmail.com
+# @Site    : http://www.leizhen.com
+# @File    : forms.py
+# @Software: PyCharm
+# code is far away from bugs with the god animal protecting
+    I love animals. They taste delicious.
+             ┏┓   ┏┓
+            ┏┛┻━━━┛┻┓
+            ┃  ☃    ┃
+            ┃ ┳┛  ┗┳ ┃
+            ┃      ┻  ┃
+            ┗━┓     ┏━┛
+              ┃     ┗━━━┓
+              ┃ 神兽保佑 ┣┓
+              ┃ 永无BUG┏┛
+              ┗┓┓┏━┳┓┏┛
+               ┃┫┫ ┃┫┫
+               ┗┻┛ ┗┻┛
+"""
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField('用户名', validators=[DataRequired()])
+    password = PasswordField('密码', validators=[DataRequired()])
+    remember_me = BooleanField('记住我')
+    submit = SubmitField('登陆')
--- app/routes.py
+++ app/routes.py
@@ -23,13 +23,15 @@
                ┃┫┫ ┃┫┫
                ┗┻┛ ┗┻┛
 """
-from flask import render_template, url_for
+from flask import render_template, url_for, flash, redirect
 
 from app import app
 
-
 # view functions 视图函数
 # 视图函数映射一个或多个 URL
+from app.forms import LoginForm
+
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -44,6 +46,14 @@ def index():
             'body': '权力的游戏'
         }
     ]
+    return render_template('index.html', title='主页', user=user, posts=posts)
 
 
-    return render_template('index.html', title='Home', user=user, posts=posts)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, remember_me='
+              f'{form.remember_me.data}')
+        return redirect(url_for('index'))
+    return render_template('login.html', title='登录', form=form)
