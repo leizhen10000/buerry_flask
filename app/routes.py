98f96f4ae1d5316cad4e322d19e9dbdb9f65962c
Lei Zhen <1027224454@qq.com>
2019-10-31 11:10:55 +0800

    在主页添加分页显示/分页浏览

--- app/routes.py
+++ app/routes.py
@@ -53,8 +53,11 @@ def index():
     page = request.args.get('page', 1, type=int)
     posts = current_user.followed_posts().paginate(
         page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('index', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None
 
-    return render_template('index.html', title='主页', form=form, posts=posts.items)
+    return render_template('index.html', title='主页', form=form, posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/login', methods=['GET', 'POST'])
