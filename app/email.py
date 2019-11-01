21de03418a6ac672aaeaa3d3c8bd3a3095d9157a
Lei Zhen <1027224454@qq.com>
2019-11-01 11:08:43 +0800

    测试同步/异步发送邮件

--- app/email.py
+++ app/email.py
@@ -40,8 +40,8 @@ def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    mail.send(msg)
-    # Thread(target=send_sync_email, args=(app, msg)).start()
+    # mail.send(msg)
+    Thread(target=send_sync_email, args=(app, msg)).start()
 
 
 def send_password_reset_email(user):
