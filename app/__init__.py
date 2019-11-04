c604089f25adeff4c065a63180c79905c1f8aefd
Lei Zhen <1027224454@qq.com>
2019-11-04 09:11:43 +0800

    添加佚名作者

--- app/__init__.py
+++ app/__init__.py
@@ -32,6 +32,7 @@ from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
+from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
@@ -44,6 +45,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 if app.debug:
     if app.config['MAIL_SERVER']:
