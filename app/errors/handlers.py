749557887ff0cb51c6b45c227000903e1ace73b3
leizhen <leizhen8080@gmail.com>
2019-11-13 17:17:10 +0800

    装饰器打错字

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -29,7 +29,7 @@ from app import db
 from app.errors import bp
 
 
-@bp.epp_rrorhandler(404)
+@bp.app_errorhandler(404)
 def not_found_error(error):
     return render_template('errors/404.html'), 404
 
